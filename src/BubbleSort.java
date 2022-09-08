0d67bcdd5bd32f2e705535a6828a5856a7769c1b
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2022-09-08 19:01:17 +0200

    Fixed freezing og GUI

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,26 +3,28 @@ import java.util.Collections;
 
 public class BubbleSort {
 	
-	// Sorting in non-increasing order 
-	public void sort(ArrayList<Integer> arr, DrawRect rectArr) throws InterruptedException {
+	double inc = 0.05;
+	// Sorting in non-increasing order of Y coordinates so that it sorted visually
+	public void sort(ArrayList<Integer> arr, DrawRect rectArr, Visualizer vis) throws InterruptedException {
 		boolean swapped;
 		for (int i = 0; i < arr.size() - 1; i++) {
 			swapped = false;
 			for (int j = 0; j < arr.size() - 1 - i; j++) {
+				Visualizer.noOfComparisons++;
 				if (arr.get(j) < arr.get(j + 1)) {
 					Collections.swap(arr, j, j + 1);
 					swapped = true;
-					Thread.sleep(15);
-					rectArr.removeAll();
-		            rectArr.setArr(arr);
-		            rectArr.revalidate();
-		            rectArr.paintImmediately(0, 230, 1620, 900);
-					System.out.println(arr);
+					Thread.sleep(30);
+					rectArr.setArr(arr);					
+		            rectArr.paintImmediately(0, 230, Visualizer.WIDTH, 900);
+		            vis.setSize(Visualizer.WIDTH, (int) (Visualizer.HEIGHT + inc));
+		            inc = -inc;
 				}
+				//System.out.println(arr);
 			}
 			if (!swapped) break;
 		}
-		Visualizer.sorted = true;
+		vis.comparisons.setText("No. of Comparisons: " + Visualizer.noOfComparisons);
 	}
 
 } 
